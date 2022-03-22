abc02531b67811733baa8a60df2f8b206d685abd
Harkiran Grewal <harkiran988@gmail.com>
2022-03-22 19:41:01 -0400

    Added list definitions for column and class lists

--- Kan-Do/KanbanBoard/KanbanBoardModel.cs
+++ Kan-Do/KanbanBoard/KanbanBoardModel.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Kan_Do.KanbanBoard
 {
-    internal class KanbanBoardModel
+    public class KanbanBoardModel
     {
+        //Defining all of the variables used in this class as private 
+        //Each private variable will have it's own getter and setter to a public variable 
+
+        //List of the KanbanCard class
+        //public List<KanbanCard> boardCards = new List<KanbanCard>();
+
+        //List of the KanbanColumn class
+        //public List<KanbanColumn> boardColumns = new List<KanbanColumn>();
+
     }
 }
