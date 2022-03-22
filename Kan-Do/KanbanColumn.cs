23286b4122cb49d7084492ecc7b411c9333cfa19
Harkiran Grewal <harkiran988@gmail.com>
2022-03-22 19:52:46 -0400

    Created data definitions for column

--- Kan-Do/KanbanColumn.cs
+++ Kan-Do/KanbanColumn.cs
@@ -8,5 +8,30 @@ namespace Kan_Do
 {
     internal class KanbanColumn
     {
+        //This class is declared as a list object in the KanbanBoardModel 
+        //(The model to the KanbanBoard UI)
+        
+        //NOTE: The m is used on private variables to represent the fact that they are members of the class
+        private string mcolumnName;
+        private int mcolumnNumber;
+        private int mcolumnId;
+
+        public string ColumnName
+        {
+            get { return mcolumnName; }
+            set { mcolumnName = value;}
+        }
+
+        public int ColumnNumber
+        {
+            get { return mcolumnNumber; }
+            set { mcolumnNumber = value;}
+        }
+
+        public int ColumnId
+        {
+            get { return mcolumnId; }
+            set { mcolumnId = value; }
+        }
     }
 }
