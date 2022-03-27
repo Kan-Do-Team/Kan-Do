63f769b00c03b52397c8bc3a40c04a00fde17fb7
nsrehman <nsamadr@gmail.com>
2022-03-27 13:07:20 -0400

    Initial Commit
    Implementation of backend and partial implementation of log in

---
+++ Kan-Do.Domain/Services/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using Kan_Do.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kan_Do.Domain.Services
+{
+    public interface IAccountService : IDataService<Account>
+    {
+        Task<Account> GetByEmail(string email);
+    }
+}
---
+++ Kan-Do.EntityFramework/Migrations/20220325115842_include-password.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kan_Do.EntityFramework.Migrations
+{
+    public partial class includepassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateJoined",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateJoined",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+        }
+    }
+}
