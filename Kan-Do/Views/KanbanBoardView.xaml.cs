1c5abf7d15122e3ab8f7321cf33889e113178cd3
nsrehman <nsamadr@gmail.com>
2022-04-04 17:29:48 -0400

    Soft-Merge
    Integrated functionalities of card_add branch

--- Kan-Do/Views/KanbanBoardView.xaml.cs
+++ Kan-Do/Views/KanbanBoardView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kan_Do.WPF.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,71 @@ namespace Kan_Do.WPF.Views
     /// </summary>
     public partial class KanbanBoardView : UserControl
     {
+        public KanbanBoardViewModel KBoardVM;
         public KanbanBoardView()
         {
             InitializeComponent();
+            KBoardVM = new KanbanBoardViewModel();
+            DataContext = KBoardVM;
+            ColumnsList.ItemsSource = KBoardVM.boardColumns;
+            ColumnsList.DataContext = KBoardVM;
+        }
+
+        private void AddColumn(object sender, RoutedEventArgs e)
+        {
+            //Call the function in the ViewModel that adds a column
+            KBoardVM.addColumn();
+        }
+
+        private void DeleteColumn(object sender, RoutedEventArgs e)
+        {
+            //Get the index of the element of the list that the Remove button was selected in
+            KanbanColumn kcol = ((Button)sender).Tag as KanbanColumn;
+
+            //Call the function in the VM, that will remove the element at the index 
+            KBoardVM.deleteColumn(kcol.ColumnNumber);
+        }
+
+        private void NewCard(object sender, RoutedEventArgs e)
+        {
+            KanbanColumn kcol = ((Button)sender).Tag as KanbanColumn;
+            //Sends ColumnID to CardDetailWindow
+            KBoardVM.cardDetails(kcol.ColumnId);
+        }
+
+        /*private void OpenCardWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Kan_Do.KanbanBoard cardWindow = new Kan_Do.KanbanBoard.CardDetailWindow();
+            //Subscribe to event that sends card data back to the board
+           // cardWindow.NewCardChanged += new EventHandler<TextEventArgs>(cardwindow_NewCardChanged);
+            cardWindow.Show();
+            //Unsubscribe to the event
+           // cardWindow.NewCardChanged -= cardWindow_NewCardChanged;
+        }*/
+
+        //The new card event
+        /*private void cardWindow_NewCardChanged(object sender, TextEventArgs e)
+        {
+           // TestEventReturn.Content = e.Text;
+            //Use __ = e. ___ to load received data into the UI 
+        }*/
+
+        private void ShiftColumnLeft(object sender, RoutedEventArgs e)
+        {
+            //Get the index of the element of the list that the Shift button was selected in
+            KanbanColumn k1col = ((Button)sender).Tag as KanbanColumn;
+
+            //Call the function in the VM, that will edit the index of the column
+            KBoardVM.shiftColumnLeft(k1col.ColumnNumber);
+        }
+
+        private void ShiftColumnRight(object sender, RoutedEventArgs e)
+        {
+            //Get the index of the element of the list that the Shift button was selected in
+            KanbanColumn k1col = ((Button)sender).Tag as KanbanColumn;
+
+            //Call the function in the VM, that will edit the index of the column
+            KBoardVM.shiftColumnRight(k1col.ColumnNumber);
         }
     }
 }
