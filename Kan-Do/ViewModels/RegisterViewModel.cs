80cda2aec7b69393044c40fee87aaf406def89a6
nsrehman <nsamadr@gmail.com>
2022-04-05 23:09:22 -0400

    test

---
+++ Kan-Do/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Kan_Do.WPF.ViewModels
+{
+    public class RegisterViewModel : ViewModelBase
+    {
+        private string _email;
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+
+        private string _firstName;
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        private string _confirmPassword;
+        public string ConfirmPassword
+        {
+            get
+            {
+                return _confirmPassword;
+            }
+            set
+            {
+                _confirmPassword = value;
+                OnPropertyChanged(nameof(ConfirmPassword));
+            }
+        }
+
+        public ICommand RegisterCommand { get; }
+
+        public ICommand ViewLoginCommand { get; }
+
+        public MessageViewModel ErrorMessageViewModel { get; }
+
+        public RegisterViewModel()
+        {
+            ErrorMessageViewModel = new MessageViewModel();
+        }
+
+    }
+}
