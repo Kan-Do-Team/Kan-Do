d28e792314c72df65fe0e26f5228f14f90624f34
nsrehman <nsamadr@gmail.com>
2022-04-12 00:43:55 -0400

    Bug Fixes
    Fixed column Id and numbers/ mcolId value when deleting.

--- Kan-Do/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Kan-Do/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -56,7 +56,7 @@ namespace Kan_Do.WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Kan-Do.WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Kan-Do.WPF;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
