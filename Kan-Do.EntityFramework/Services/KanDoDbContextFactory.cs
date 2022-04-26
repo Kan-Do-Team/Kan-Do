51bad3a9a98faf4434c5a162291df04ba54cfd3f
nsrehman <nsamadr@gmail.com>
2022-04-26 14:52:58 -0400

    Update gitignore

--- Kan-Do.EntityFramework/Services/KanDoDbContextFactory.cs
+++ Kan-Do.EntityFramework/Services/KanDoDbContextFactory.cs
@@ -4,8 +4,6 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kan_Do.EntityFramework.Services
 {
